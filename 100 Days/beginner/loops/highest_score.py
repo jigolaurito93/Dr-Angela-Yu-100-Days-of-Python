a746186f4d5629c03e2f794de25ba0a1a5b8ec51
Jose Laurito <jigolaurito93@yahoo.com>
2023-07-25 01:02:22 -0500

    finished highest score project

---
+++ 100 Days/beginner/loops/highest_score.py
@@ -0,0 +1,30 @@
+# Instructions
+# You are going to write a program that calculates the highest score from a List of scores.
+
+# e.g. student_scores = [78, 65, 89, 86, 55, 91, 64, 89]
+
+# Important you are not allowed to use the max or min functions. The output words must match the example. i.e
+
+# The highest score in the class is: x
+
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+# Set up a for loop to run each score
+# Set up a variable named "highest score" with a zero value temporarily
+# Use comparison operator for each score and if the score is greater than the current value, that will be the new highest score
+# print the higest score
+
+highest_score = 0
+
+for score in student_scores:
+  if score > highest_score:
+    highest_score = score
+
+print(f"The highest score in the class is: {highest_score}")
