14ccbb1834eeef328e7b26eab990b2406db358a5
Jose Laurito <jigolaurito93@yahoo.com>
2023-07-26 02:29:29 -0500

    import ASCII word art and word list from different python file

--- 100 Days/beginner/build hangman game/hangman5.py
+++ 100 Days/beginner/build hangman game/hangman5.py
@@ -1,6 +1,10 @@
 #Step 5
 
 import random
+from hangman_words import word_list
+from hangman_art import stages, logo
+
+
 
 #TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
 #Delete this line: word_list = ["ardvark", "baboon", "camel"]
@@ -11,9 +15,19 @@ end_of_game = False
 lives = 6
 
 #TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+print(logo)
 
-#Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+
+
+
+#Testing code /// Uncomment this line if you want to see the word
+# =================================================================
+# print(f'Pssst, the solution is {chosen_word}.')
+# =================================================================
+
+
+
+
 
 #Create blanks
 display = []
@@ -24,17 +38,19 @@ while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
     #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
-
+    if guess in display:
+        print(f"You've already guessed {guess} ")
     #Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
-        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
 
     #Check if user is wrong.
     if guess not in chosen_word:
         #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
         lives -= 1
         if lives == 0:
             end_of_game = True
